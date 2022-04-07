fa87e5551e8a2dcccb4d083b38cf8b22138c7743
turtle-insect <turtle.insect@gmail.com>
2022-04-08 00:02:09 +0900

    first commit

---
+++ ChronoCross/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace ChronoCross
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_PreviewDragOver(object sender, DragEventArgs e)
+		{
+			e.Handled = e.Data.GetDataPresent(DataFormats.FileDrop);
+		}
+
+		private void Window_Drop(object sender, DragEventArgs e)
+		{
+			String[]? files = e.Data.GetData(DataFormats.FileDrop) as String[];
+			if (files == null) return;
+
+			FileOpen(files[0], false);
+		}
+
+		private void MenuItemFileOpen_Click(object sender, RoutedEventArgs e)
+		{
+			FileOpen(false);
+		}
+
+		private void MenuItemFileForceOpen_Click(object sender, RoutedEventArgs e)
+		{
+			FileOpen(true);
+		}
+
+		private void MenuItemFileSave_Click(object sender, RoutedEventArgs e)
+		{
+			SaveData.Instance().Save();
+		}
+
+		private void MenuItemFileExit_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+
+		private void ComboBoxFile_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			DataContext = new ViewModel();
+		}
+
+		private void FileOpen(bool force)
+		{
+			var dlg = new OpenFileDialog();
+			dlg.Filter = "memcard|memcard*.mcr";
+			if (dlg.ShowDialog() == false) return;
+			FileOpen(dlg.FileName, force);
+		}
+
+		private void FileOpen(String filename, bool force)
+		{
+			if (SaveData.Instance().Open(filename, force) == false)
+			{
+				MessageBox.Show("CheckSum Error", "File Open", MessageBoxButton.OK, MessageBoxImage.Error);
+				return;
+			}
+			DataContext = new ViewModel();
+		}
+	}
+}
---
+++ ChronoCross/ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChronoCross
+{
+	internal class ViewModel
+	{
+		public uint Money
+		{
+			get => SaveData.Instance().ReadNumber(0x58C, 4);
+			set => Util.WriteNumber(0x58C, 4, value, 0, 9999999);
+		}
+
+		public uint FileIndex
+		{
+			get => SaveData.Instance().Adventure;
+			set
+			{
+				SaveData.Instance().UpdateCheckSum();
+				SaveData.Instance().Adventure = value;
+			}
+		}
+	}
+}
